fa6732b99a9710abf496c505f279b71c6bf91004
Cristina Lugo <cmlugoce@syr.edu>
2018-06-17 12:04:30 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -28,20 +28,23 @@ class Owner
   end
   
   def buy_fish(name)
-    pets[:fishes] << Fish.new(name)
+    @pets[:fishes] << Fish.new(name)
   end
   
   def buy_cat(name)
-    pets[:cats] << Cat.new(name)
+    @pets[:cats] << Cat.new(name)
   end
   
   def buy_dog(name)
-    pets[:dogs] << Dog.new(name)
+    @pets[:dogs] << Dog.new(name)
   end  
   
   def walk_dogs
     @pets[:dogs].each{|dog| dog.mood = "happy"}
   end 
-    
+  
+  def play_with_cats
+    @pets[:cats].each{|cat| cat.mood = "happy"}
+  end
     
 end
